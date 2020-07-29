6954a3618ea2a3d3c2be854307e89e5bd96efc28
Florian Wenzel <mail@kreadiv.de>
2020-07-29 21:35:01 +0200

    Add bundle class

---
+++ src/ContaoProfilesBundle.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+/*
+ * Contao profiles extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2020, Florian Wenzel
+ * @author     Florian Wenzel <mail@kreadiv.de>
+ * @license    LGPL-3.0+
+ */
+
+namespace Kreadiv\ContaoProfilesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoProfilesBundle extends Bundle
+{
+}
