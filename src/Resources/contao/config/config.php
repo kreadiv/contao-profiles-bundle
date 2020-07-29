ac3b9fb8b5e211641a54ecdc43e380e250c012c7
Florian Wenzel <mail@kreadiv.de>
2020-07-29 22:34:26 +0200

    Update config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,12 @@
 <?php
 
 $GLOBALS['FE_MOD']['cp']['profileListing'] = 'Kreadiv\ContaoProfilesBundle\Module\ProfileListingModule';
+
+// Backend modules
+array_insert($GLOBALS['BE_MOD'], 1, [
+    'cp' => [
+        'cp_profiles' => [
+            'tables' => ['tl_cp_profiles']
+        ]
+    ]
+]);
