d5a893f2e5a1dda49e40c2ae06a3fd64943ecc13
Florian Wenzel <mail@kreadiv.de>
2020-07-29 22:13:14 +0200

    Fields added

--- src/Resources/contao/dca/tl_cp_profiles.php
+++ src/Resources/contao/dca/tl_cp_profiles.php
@@ -58,6 +58,7 @@ $GLOBALS['TL_DCA']['tl_cp_profiles'] = [
             'sql'                => "int(10) unsigned NOT NULL default 0"
         ],
         'first_name' => [
+            'label'              => &$GLOBALS['TL_LANG']['tl_cp_profiles']['first_name'],
             'exclude'            => true,
             'search'             => true,
             'sorting'            => true,
@@ -67,6 +68,17 @@ $GLOBALS['TL_DCA']['tl_cp_profiles'] = [
             'sql'                => "varchar(255) NOT NULL default ''"
         ],
         'last_name' => [
+            'label'              => &$GLOBALS['TL_LANG']['tl_cp_profiles']['last_name'],
+            'exclude'            => true,
+            'search'             => true,
+            'sorting'            => true,
+            'flag'               => 1,
+            'inputType'          => 'text',
+            'eval'               => ['mandatory' => true, 'maxlength' => 255, 'decodeEntities' => true, 'tl_class' => 'w50'],
+            'sql'                => "varchar(255) NOT NULL default ''"
+        ],
+        'department' => [
+            'label'              => &$GLOBALS['TL_LANG']['tl_cp_profiles']['department'],
             'exclude'            => true,
             'search'             => true,
             'sorting'            => true,
@@ -76,6 +88,7 @@ $GLOBALS['TL_DCA']['tl_cp_profiles'] = [
             'sql'                => "varchar(255) NOT NULL default ''"
         ],
         'email' => [
+            'label'              => &$GLOBALS['TL_LANG']['tl_cp_profiles']['email'],
             'exclude'            => true,
             'search'             => true,
             'sorting'            => true,
@@ -83,6 +96,23 @@ $GLOBALS['TL_DCA']['tl_cp_profiles'] = [
             'inputType'          => 'text',
             'eval'               => ['mandatory' => true, 'rgxp' => 'email', 'maxlength' => 255, 'decodeEntities' => true, 'tl_class' => 'w50'],
             'sql'                => "varchar(255) NOT NULL default ''"
+        ],
+        'phone' => [
+            'label'              => &$GLOBALS['TL_LANG']['tl_cp_profiles']['phone'],
+            'exclude'            => true,
+            'search'             => true,
+            'sorting'            => true,
+            'flag'               => 1,
+            'inputType'          => 'text',
+            'eval'               => ['mandatory' => true, 'rgxp' => 'phone', 'maxlength' => 255, 'decodeEntities' => true, 'tl_class' => 'w50'],
+            'sql'                => "varchar(255) NOT NULL default ''"
+        ],
+        'profile' => [
+            'label'              => &$GLOBALS['TL_LANG']['tl_cp_profiles']['profile'],
+            'exclude'            => true,
+            'inputType'          => 'textarea',
+            'eval'               => ['mandatory' => false, 'rte' => 'tinyMCE', 'decodeEntities' => true, 'tl_class' => 'clr', 'allowHtml' => true],
+            'sql'                => "text NULL"
         ]
     ]
 ];
