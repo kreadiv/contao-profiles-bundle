f86000676b0918ff5aabead898881f53e4f218e9
Florian Wenzel <mail@kreadiv.de>
2020-07-29 15:25:32 +0200

    Add config and list

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,4 +3,48 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['cp_profiles'] = '{title_legend},name,headline,type,cs_productLimit;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_cp_profiles'] = [
+
+    // Config
+    'config' => [
+        'dataContainer'                  => 'Table',
+        'enableVersioning'               => true,
+        'sql' => [
+            'keys' => [
+                'id'     => 'primary',
+                'email'  => 'unique',
+                'email'  => 'index',
+                'active' => 'index'
+            ]
+        ]
+    ],
+
+    // List
+    'list' => [
+        'sorting' => [
+            'mode'               => 4,
+            'fields'             => ['first_name', 'last_name'],
+            'panelLayout'        => 'filter;sort,search,limit',
+            'headerFields'       => ['title', 'jumpTo', 'tstamp']
+        ],
+        'operations' => [
+            'edit' => [
+                'href'           => 'act=edit',
+                'icon'           => 'edit.svg'
+            ],
+            'copy' => [
+                'href'           => 'act=paste&amp;mode=copy',
+                'icon'           => 'copy.svg'
+            ],
+            'delete' => [
+                'href'           => 'act=delete',
+                'icon'           => 'delete.svg',
+                'attributes'     => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+            ],
+            'show' => [
+                'href'           => 'act=show',
+                'icon'           => 'show.svg'
+            ]
+        ]
+    ]
+];
