191d8462befe24781fe4afef19c6da51068580ae
Florian Wenzel <mail@kreadiv.de>
2020-08-04 09:33:08 +0200

    Update

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -3,7 +3,7 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['cp_profileReader'] = '{title_legend},name,type;{cp_profile_legend},cp_profile';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['cp_profileReader'] = '{title_legend},name,type;{cp_profile_legend},cp_profile,cp_profile_slim';
 
 /**
 * Fields
@@ -19,4 +19,12 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['cp_profile'] = [
     'options'                     => $GLOBALS['TL_LANG']['tl_module']['cp_profileOptions']['options'],
     'eval'                        => ['includeBlankOption' => true, 'chosen' => true, 'tl_class' => 'w50'],
     'sql'                         => "varchar(255) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['cp_profile_slim'] = [
+    'label'                       => &$GLOBALS['TL_LANG']['tl_module']['cp_profile_slim'],
+    'exclude'                     => true,
+    'inputType'                   => 'checkbox',
+    'eval'                        => ['includeBlankOption' => true, 'tl_class' => 'w50'],
+    'sql'                         => "INT unsigned NULL",
 ];
