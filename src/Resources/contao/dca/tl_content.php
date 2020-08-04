c49a7282197c2c82949860ff7c1cc09b1f7dbee4
Florian Wenzel <mail@kreadiv.de>
2020-08-04 11:14:09 +0200

    Add translations. Update DCA

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -3,23 +3,25 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_content']['palettes']['cp_profileReader'] = '{title_legend},name,type;{cp_profileLegend},cp_profile,cp_profileWithDescription;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['cp_profileReader'] = '{cp_profileReaderLegend},name,type;{cp_settingsLegend},cp_profile,cp_profileWithDescription';
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['cp_profileList'] = '{cp_profileListLegend},name,type';
 
 /**
-* Fields
-*/
+ * Fields
+ */
 $GLOBALS['TL_DCA']['tl_content']['fields']['cp_profile'] = [
     'label'                       => &$GLOBALS['TL_LANG']['tl_content']['cp_profile'],
     'exclude'                     => true,
     'inputType'                   => 'select',
     'options_callback'            => function () {
-        $objProfiles = \Database::getInstance()->prepare('SELECT id, first_name, last_name FROM tl_cp_profiles ORDER BY last_name');
+        $objProfiles = \Database::getInstance()->prepare('SELECT id, first_name, last_name FROM tl_cp_profiles ORDER BY last_name')->execute();
 
-        dd($objProfiles);
         $profileOptions = [];
-        foreach ($objProfiles->data as $profile) {
-            $profileOptions[$profile['id']] = $profile['first_name'] . ' ' . $profile['last_name'];
+        while ($objProfiles->next()) {
+            $profileOptions[$objProfiles->id] = $objProfiles->first_name . ' ' . $objProfiles->last_name;
         }
+
         return $profileOptions;
     },
     'options'                     => $GLOBALS['TL_LANG']['tl_content']['cp_profileOptions']['options'],
@@ -31,6 +33,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['cp_profileWithDescription'] = [
     'label'                       => &$GLOBALS['TL_LANG']['tl_content']['cp_profileWithDescription'],
     'exclude'                     => true,
     'inputType'                   => 'checkbox',
-    'eval'                        => ['includeBlankOption' => true, 'tl_class' => 'w50'],
+    'eval'                        => ['includeBlankOption' => true, 'tl_class' => 'w50 clr'],
     'sql'                         => "char(1) NOT NULL default ''",
 ];
