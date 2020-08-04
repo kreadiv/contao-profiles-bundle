1d6a0854f021d234d84069e94884a4fcd2fd6859
Florian Wenzel <mail@kreadiv.de>
2020-08-04 10:11:06 +0200

    Translations added

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -3,7 +3,7 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_content']['palettes']['cp_profileReader'] = '{title_legend},name,type;{cp_profileLegend},cp_profile,cp_profile_slim;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['cp_profileReader'] = '{title_legend},name,type;{cp_profileLegend},cp_profile,cp_profileWithDescription;{expert_legend:hide},guests,cssID,space';
 
 /**
 * Fields
@@ -14,15 +14,20 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['cp_profile'] = [
     'inputType'                   => 'select',
     'options_callback'            => function () {
         $objProfiles = \Database::getInstance()->prepare('SELECT id, first_name, last_name FROM tl_cp_profiles ORDER BY last_name');
-        return $objProfiles;
+
+        $profileOptions = [];
+        foreach ($objProfiles->data as $profile) {
+            $profileOptions[$profile['id']] = $profile['first_name'] . ' ' . $profile['last_name'];
+        }
+        return $profileOptions;
     },
     'options'                     => $GLOBALS['TL_LANG']['tl_content']['cp_profileOptions']['options'],
     'eval'                        => ['includeBlankOption' => true, 'chosen' => true, 'tl_class' => 'w50'],
     'sql'                         => "varchar(255) NOT NULL default ''",
 ];
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['cp_profile_slim'] = [
-    'label'                       => &$GLOBALS['TL_LANG']['tl_content']['cp_profile_slim'],
+$GLOBALS['TL_DCA']['tl_content']['fields']['cp_profileWithDescription'] = [
+    'label'                       => &$GLOBALS['TL_LANG']['tl_content']['cp_profileWithDescription'],
     'exclude'                     => true,
     'inputType'                   => 'checkbox',
     'eval'                        => ['includeBlankOption' => true, 'tl_class' => 'w50'],
