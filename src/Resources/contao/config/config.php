24ee3313c11aae068e0b014bd01d962065e22724
Florian Wenzel <mail@kreadiv.de>
2020-09-01 20:33:21 +0200

    Profile list update

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,6 +2,7 @@
 
 // Content elements
 $GLOBALS['TL_CTE']['cp']['cp_profileReader'] = 'Kreadiv\ContaoProfilesBundle\Element\ProfileReaderElement';
+$GLOBALS['TL_CTE']['cp']['cp_profileList'] = 'Kreadiv\ContaoProfilesBundle\Element\ProfileListElement';
 
 // Backend modules
 array_insert($GLOBALS['BE_MOD'], 1, [
