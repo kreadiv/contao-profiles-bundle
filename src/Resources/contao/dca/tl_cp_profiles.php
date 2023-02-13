d9682ebe79e4971bc39ddc130c31b26bf1f9950e
Florian Wenzel <mail@kreadiv.de>
2023-02-13 22:26:12 +0100

    Fix PHP Warning: undefined array key "deleteConfirm"

--- src/Resources/contao/dca/tl_cp_profiles.php
+++ src/Resources/contao/dca/tl_cp_profiles.php
@@ -38,7 +38,7 @@ $GLOBALS['TL_DCA']['tl_cp_profiles'] = [
             'delete' => [
                 'href'           => 'act=delete',
                 'icon'           => 'delete.svg',
-                'attributes'     => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'     => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ],
             'show' => [
                 'href'           => 'act=show',
